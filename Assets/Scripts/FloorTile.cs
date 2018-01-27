da9a96b168d18e0ec8e0b14ffaffd8c52936aeba
Andres Moreno <andres.morenog@udea.edu.co>
2018-01-27 10:53:22 -0500

    Level1

---
+++ Assets/Scripts/FloorTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FloorTile : Tile {
+
+    [SerializeField]
+    private Sprite[] floorSprites;
+
+    [SerializeField]
+    private Sprite preview;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
